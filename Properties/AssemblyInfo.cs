d52c0ad9baeac7bc2732f654cd86fca4c8970838
wolf109909 <84360921+wolf109909@users.noreply.github.com>
2022-07-19 15:14:02 +0800

    forgot something important

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("北极星CN一键启动器")]
-[assembly: AssemblyCopyright("Copyright © wolf109909 2022")]
+[assembly: AssemblyCopyright("Copyright © EladNLG & wolf109909 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
